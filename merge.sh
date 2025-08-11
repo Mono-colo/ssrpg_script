bd383906e95e751a43b7cb581ce1635f849f10c9
BigNewYork <sunkangju7@163.com>
2025-08-11 15:54:53 +0800

    add scrpts/ui

--- merge.sh
+++ merge.sh
@@ -46,8 +46,9 @@ regex_replace_file() {
 
 remove_file my_script
 
-append_tool ./scripts/tool                   my_script    
 append_tool ./scripts/define                 my_script    
+append_tool ./scripts/ui                     my_script    
+append_tool ./scripts/tool                   my_script    
 append_tool ./scripts/loc_1_temple           my_script    
 append_tool ./scripts/loc_2_icy_ridge        my_script    
 append_tool ./scripts/loc_3_bronze_mine      my_script    
@@ -63,3 +64,34 @@ regex_replace_file my_script  "^\s*\n"  ""
 regex_replace_file my_script  "//.*"  "" 
 regex_replace_file my_script  "/\*[\s\S]*?\*/\n"  "" 
 regex_replace_file my_script  "\n^$"  "" 
+
+
+
+
+
+
+
+# remove_file my_main
+
+# append_tool ./scripts/define                 my_main   
+# echo "import my_scripts/ui                   " >> my_main
+# echo "import my_scripts/tool                 " >> my_main
+# echo "import my_scripts/loc_1_temple         " >> my_main
+# echo "import my_scripts/loc_2_icy_ridge      " >> my_main
+# echo "import my_scripts/loc_3_bronze_mine    " >> my_main
+# echo "import my_scripts/loc_4_undead_crypt   " >> my_main
+# echo "import my_scripts/loc_5_fungus_forest  " >> my_main
+# echo "import my_scripts/loc_6_caustic_caves  " >> my_main
+# echo "import my_scripts/loc_7_deadwood_valley" >> my_main
+# echo "import my_scripts/loc_8_rocky_plateau  " >> my_main
+
+# mv ./scripts/ui                     ${ssrpg_scripts_path}/ui                  
+# mv ./scripts/tool                   ${ssrpg_scripts_path}/tool                  
+# mv ./scripts/loc_1_temple           ${ssrpg_scripts_path}/loc_1_temple          
+# mv ./scripts/loc_2_icy_ridge        ${ssrpg_scripts_path}/loc_2_icy_ridge       
+# mv ./scripts/loc_3_bronze_mine      ${ssrpg_scripts_path}/loc_3_bronze_mine     
+# mv ./scripts/loc_4_undead_crypt     ${ssrpg_scripts_path}/loc_4_undead_crypt    
+# mv ./scripts/loc_5_fungus_forest    ${ssrpg_scripts_path}/loc_5_fungus_forest   
+# mv ./scripts/loc_6_caustic_caves    ${ssrpg_scripts_path}/loc_6_caustic_caves   
+# mv ./scripts/loc_7_deadwood_valley  ${ssrpg_scripts_path}/loc_7_deadwood_valley 
+# mv ./scripts/loc_8_rocky_plateau    ${ssrpg_scripts_path}/loc_8_rocky_plateau   
